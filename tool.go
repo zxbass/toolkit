15e27978d2039d697aafc030cef926e0f84f7ddf
Alexander Zabolotskiy <brainexploded@gmail.com>
2026-02-02 21:55:33 +0300

    added some new methods

--- tool.go
+++ tool.go
@@ -1,6 +1,8 @@
 package toolkit
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
@@ -28,8 +30,10 @@ var (
 )
 
 type Tools struct {
-	MaxFileSize      int
-	AllowedFileTypes []string
+	MaxFileSize            int
+	AllowedFileTypes       []string
+	MaxJSONSize            int
+	JSONAllowUnknownFields bool
 }
 
 func (t *Tools) RandomString(n int) string {
@@ -221,4 +225,126 @@ func (t *Tools) DownloadStaticFile(w http.ResponseWriter, r *http.Request, path,
 	)
 
 	http.ServeFile(w, r, fp)
+}
+
+type JSONResponse struct {
+	Error   bool   `json:"error"`
+	Message string `json:"message"`
+	Data    any    `json:"data,omitempty"`
+}
+
+func (t *Tools) ReadJSON(w http.ResponseWriter, r *http.Request, data any) error {
+	maxBytes := t.MaxJSONSize
+	if maxBytes == 0 {
+		maxBytes = 1024 * 1024
+	}
+
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+	dec := json.NewDecoder(r.Body)
+
+	if !t.JSONAllowUnknownFields {
+		dec.DisallowUnknownFields()
+	}
+
+	err := dec.Decode(data)
+	if err != nil {
+		var syntaxError *json.SyntaxError
+		var unmarshalTypeError *json.UnmarshalTypeError
+		var invalidUnmarshalError *json.InvalidUnmarshalError
+
+		switch {
+		case errors.As(err, &syntaxError):
+			return fmt.Errorf("body contains badly formed JSON at character %d", syntaxError.Offset)
+		case errors.Is(err, io.ErrUnexpectedEOF):
+			return errors.New("body contains badly formed JSON")
+		case errors.As(err, &unmarshalTypeError):
+			if unmarshalTypeError.Field != "" {
+				return fmt.Errorf("body contains incorrect JSON type for field %v", &unmarshalTypeError.Field)
+			}
+			return fmt.Errorf("body contains incorrect JSON type at character %d", unmarshalTypeError.Offset)
+		case errors.Is(err, io.EOF):
+			return errors.New("body must not be empty")
+		case strings.HasPrefix(err.Error(), "json: unknown field"):
+			fieldName := strings.TrimPrefix(err.Error(), "json: unknown field")
+			return fmt.Errorf("body contains unknown key %s", fieldName)
+		case err.Error() == "http: request body too large":
+			return fmt.Errorf("body must not be larger than %d bytes", maxBytes)
+		case errors.As(err, &invalidUnmarshalError):
+			return fmt.Errorf("error unmarshalling JSON: %s", err.Error())
+		default:
+			return err
+		}
+	}
+
+	err = dec.Decode(&struct{}{})
+	if err != io.EOF {
+		return errors.New("body must contain exactly one JSON object")
+	}
+
+	return nil
+}
+
+func (t *Tools) WriteJSON(w http.ResponseWriter, status int, data any, headers ...http.Header) error {
+	out, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	if len(headers) > 0 {
+		for k, v := range headers[0] {
+			w.Header()[k] = v
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+
+	_, err = w.Write(out)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (t *Tools) ErrorJSON(w http.ResponseWriter, err error, status ...int) error {
+	statusCode := http.StatusBadRequest
+
+	if len(status) > 0 {
+		statusCode = status[0]
+	}
+
+	var payload = JSONResponse{
+		Error:   true,
+		Message: err.Error(),
+	}
+
+	return t.WriteJSON(w, statusCode, payload)
+}
+
+func (t *Tools) PushJSONToRemote(uri string, data any, client ...*http.Client) (*http.Response, int, error) {
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	httpClient := &http.Client{}
+	if len(client) > 0 {
+		httpClient = client[0]
+	}
+
+	request, err := http.NewRequest("POST", uri, bytes.NewBuffer(jsonData))
+	if err != nil {
+		return nil, 0, err
+	}
+
+	request.Header.Set("Content-Type", "application/json")
+
+	response, err := httpClient.Do(request)
+	if err != nil {
+		return nil, 0, err
+	}
+	defer response.Body.Close()
+
+	return response, response.StatusCode, nil
 }
--- tool_test.go
+++ tool_test.go
@@ -1,11 +1,15 @@
 package toolkit
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"image"
 	"image/jpeg"
 	"io"
 	"io/ioutil"
+
 	"mime/multipart"
 	"net/http"
 	"net/http/httptest"
@@ -13,6 +17,40 @@ import (
 	"sync"
 	"testing"
 )
+
+type RoundTripFunc func(req *http.Request) *http.Response
+
+func (f RoundTripFunc) RoundTrip(req *http.Request) (*http.Response, error) {
+	return f(req), nil
+}
+
+func NewTestClient(fn RoundTripFunc) *http.Client {
+	return &http.Client{
+		Transport: fn,
+	}
+}
+
+func TestTools_PushJSONToRemote(t *testing.T) {
+	client := NewTestClient(func(req *http.Request) *http.Response {
+		return &http.Response{
+			StatusCode: http.StatusOK,
+			Body:       ioutil.NopCloser(bytes.NewBufferString("normaldy")),
+			Header:     make(http.Header),
+		}
+	})
+
+	var tools Tools
+
+	var foo struct {
+		Bar string `json:"bar"`
+	}
+	foo.Bar = "bar"
+
+	_, _, err := tools.PushJSONToRemote("http://example.com/test", foo, client)
+	if err != nil {
+		t.Error(err)
+	}
+}
 
 func TestTools_RandomString(t *testing.T) {
 	var testTools Tools
@@ -224,9 +262,105 @@ func TestTools_DownloadStaticFile(t *testing.T) {
 		t.Errorf("wrong content disposition [%s]", res.Header.Get("Content-Disposition"))
 	}
 
-	_, err := ioutil.ReadAll(res.Body)
+	_, err := io.ReadAll(res.Body)
 	if err != nil {
 		t.Error(err)
 	}
+}
 
+var JSONTests = []struct {
+	name          string
+	json          string
+	errorExpected bool
+	maxSize       int
+	allowUnknown  bool
+}{
+	{name: "valid json", json: `{"foo": "bar"}`, errorExpected: false, maxSize: 1024, allowUnknown: false},
+	{name: "invalid json", json: `{"foo": }`, errorExpected: true, maxSize: 1024, allowUnknown: false},
+	{name: "wrong json type", json: `{"foo": 1}`, errorExpected: true, maxSize: 1024, allowUnknown: false},
+	{name: "too many objects", json: `{"foo": "bar"}{"foo": "baz"}`, errorExpected: true, maxSize: 1024, allowUnknown: false},
+	{name: "empty body", json: ``, errorExpected: true, maxSize: 1024, allowUnknown: false},
+	{name: "syntax error", json: `{"foo": "bar}`, errorExpected: true, maxSize: 1024, allowUnknown: false},
+	{name: "unknown field", json: `{"x": "bar"}`, errorExpected: true, maxSize: 1024, allowUnknown: false},
+	{name: "unknown fields allowed", json: `{"x": "bar"}`, errorExpected: false, maxSize: 1024, allowUnknown: true},
+	{name: "missing field name", json: `{x: "bar"}`, errorExpected: true, maxSize: 1024, allowUnknown: true},
+	{name: "body is too large", json: `{"foo": "bar"}`, errorExpected: true, maxSize: 5, allowUnknown: false},
+	{name: "not a json", json: `lololo`, errorExpected: true, maxSize: 1024, allowUnknown: false},
+}
+
+func TestTools_ReadJSON(t *testing.T) {
+	var tools Tools
+
+	for _, test := range JSONTests {
+		tools.MaxJSONSize = test.maxSize
+		tools.JSONAllowUnknownFields = test.allowUnknown
+
+		var decodedJSON struct {
+			Foo string `json:"foo"`
+		}
+
+		req, err := http.NewRequest("POST", "/", bytes.NewReader([]byte(test.json)))
+		if err != nil {
+			t.Error(err)
+		}
+		defer req.Body.Close()
+
+		rr := httptest.NewRecorder()
+
+		err = tools.ReadJSON(rr, req, &decodedJSON)
+		if test.errorExpected && err == nil {
+			t.Error("error expected, none received")
+		}
+
+		if !test.errorExpected && err != nil {
+			t.Errorf("error was not expected, but received one: %s", err.Error())
+		}
+	}
+}
+
+func TestTools_WriteJSON(t *testing.T) {
+	var tools Tools
+
+	rr := httptest.NewRecorder()
+	payload := JSONResponse{
+		Error:   false,
+		Message: "foo",
+	}
+
+	headers := make(http.Header)
+	headers.Add("Foo", "Bar")
+
+	err := tools.WriteJSON(rr, http.StatusOK, payload, headers)
+	if err != nil {
+		t.Errorf("WriteJSON errored with error: %s", err.Error())
+	}
+}
+
+func TestTools_ErrorJSON(t *testing.T) {
+	var tools Tools
+
+	rr := httptest.NewRecorder()
+	err := tools.ErrorJSON(rr, errors.New("Foo"), http.StatusServiceUnavailable)
+	if err != nil {
+		t.Errorf("ErrorJSON failed: %v", err)
+	}
+
+	var payload JSONResponse
+	decoder := json.NewDecoder(rr.Body)
+	err = decoder.Decode(&payload)
+	if err != nil {
+		t.Error("Received error while decoding json", err)
+	}
+
+	if !payload.Error {
+		t.Error("Error set to false, should be true")
+	}
+
+	if rr.Code != http.StatusServiceUnavailable {
+		t.Errorf("Status set to %v, should be %v", rr.Code, http.StatusServiceUnavailable)
+	}
+
+	if payload.Message != "Foo" {
+		t.Errorf("Message set to %s, should be %s", payload.Message, "Foo")
+	}
 }
